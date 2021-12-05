1a365f774e57893d79279e19f3e3cab56b3e7c12
Tanner Bacon <tannerbacon20@gmail.com>
2021-12-05 00:13:06 -0700

    redirecting to port 3001

--- back-end/index.js
+++ back-end/index.js
@@ -88,7 +88,7 @@ app.delete('/api/reviews/:id', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3001, () => console.log('Server listening on port 3000!'));
 
 
 
